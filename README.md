a14f7cddbc211454c0c08f8d020c9de083fb4903
5tupidmuffin <rameshwark9500@gmail.com>
2021-12-26 12:45:16 +0530

    prettified

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SyncMe
+
 A website to Generate Time-synced Lyrics.
 https://5tupidmuffin.github.io/SyncMe/
--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // input fields
-const lyrics = document.querySelector('#original_lrc');
-const verse = document.querySelector('#verse');
-const stamp = document.querySelector('#timestamp');
-const synced_lyrics = document.querySelector('#synced_lrc');
+const lyrics = document.querySelector("#original_lrc");
+const verse = document.querySelector("#verse");
+const stamp = document.querySelector("#timestamp");
+const synced_lyrics = document.querySelector("#synced_lrc");
 
 // button
-const start = document.querySelector('#start');
-const reset = document.querySelector('#reset');
-const sync = document.querySelector('#sync');
-const cpbtn = document.querySelector('#cpbtn');
+const start = document.querySelector("#start");
+const reset = document.querySelector("#reset");
+const sync = document.querySelector("#sync");
+const cpbtn = document.querySelector("#cpbtn");
 
 // other
 let flag = null;
 let isRunning = false;
-let lrc = '';
+let lrc = "";
 let currentLine = 0;
 
 const displayVerse = (lyricsArray, lineNo) => {
@@ -22,15 +22,15 @@ const displayVerse = (lyricsArray, lineNo) => {
   } else {
     clearInterval(flag);
   }
-}
+};
 
 // Button Events
 lyrics.onpaste = () => {
   setTimeout(() => {
     lrc = lyrics.value;
-    displayVerse(lrc.split('\n'), currentLine);
+    displayVerse(lrc.split("\n"), currentLine);
   }, 20);
-}
+};
 
 start.onclick = () => {
   if (!lyrics.value) {
@@ -39,30 +39,34 @@ start.onclick = () => {
   }
   setTimeout(() => {
     lrc = lyrics.value;
-    displayVerse(lrc.split('\n'), currentLine);
+    displayVerse(lrc.split("\n"), currentLine);
   }, 20);
 
-  let mins = 0, secs = 0, millis = 0;
+  let mins = 0,
+    secs = 0,
+    millis = 0;
   isRunning = true;
   flag = setInterval(() => {
     millis += 10;
     if (millis === 1000) {
       millis = 0;
       secs += 1;
-    } 
+    }
     if (secs === 60) {
       secs = 0;
       mins += 1;
     }
-    stamp.textContent = `${mins < 9 ? '0' + mins : mins}:${secs < 9 ? '0' + secs : secs}:${(millis / 10) < 9 ? '0' + (millis / 10) : millis / 10}`
-  }, 10)
-}
+    stamp.textContent = `${mins < 9 ? "0" + mins : mins}:${
+      secs < 9 ? "0" + secs : secs
+    }:${millis / 10 < 9 ? "0" + millis / 10 : millis / 10}`;
+  }, 10);
+};
 
 reset.onclick = () => {
   isRunning = false;
   clearInterval(flag);
-  stamp.textContent = '00:00:00'
-}
+  stamp.textContent = "00:00:00";
+};
 
 sync.onclick = () => {
   if (!lyrics.value) {
@@ -70,13 +74,13 @@ sync.onclick = () => {
     return;
   }
   synced_lyrics.value += `[${stamp.textContent}] ${verse.value}\n`;
-  displayVerse(lrc.split('\n'), currentLine += 1);
-}
+  displayVerse(lrc.split("\n"), (currentLine += 1));
+};
 
 cpbtn.onclick = () => {
   if (synced_lyrics.value) {
     navigator.clipboard.writeText(synced_lyrics.value);
   } else {
-    alert("Nothing to Copy!")
+    alert("Nothing to Copy!");
   }
-}
+};
